96c9f7a0e0cceace307202d28eba9cabf3f0838a
fatlard1993 <fatlard1993@gmail.com>
2019-03-12 12:58:43 -0700

    cleanup and fix from doubling up on npm i

--- src/zelda.js
+++ src/zelda.js
@@ -6,60 +6,38 @@ const findRoot = require('find-root');
 const rimraf = require('rimraf');
 
 function getCodePackages(codePath){
-	let entries;
-
 	try{
-		entries = fs.readdirSync(codePath);
+		const packages = {};
+
+		fs.readdirSync(codePath).forEach((entry) => {
+			const pkgPath = path.join(codePath, entry);
+
+			try{ packages[require(path.join(pkgPath, 'package.json')).name] = pkgPath; }
+
+			catch(err){ return; }
+		});
+
+		return packages;
 	}
 
-	catch(err){
-		throw new Error(`Could not find ${codePath} | ${err.message}`);
-	}
-
-	const packages = {};
-
-	entries.forEach(function(entry){
-		const pkgPath = path.join(codePath, entry);
-
-		try{
-			const pkg = require(path.join(pkgPath, 'package.json'));
-
-			packages[pkg.name] = pkgPath;
-		}
-
-		catch(err){
-			return; // ignore folders without package.json
-		}
-	});
-
-	return packages;
+	catch(err){ throw new Error(`Could not find ${codePath} | ${err.message}`); }
 }
 
 function traverseNodeModules(pkgPath, cb){
-	const modulesPath = path.join(pkgPath, 'node_modules');
-	let entries;
-
 	try{
-		entries = fs.readdirSync(modulesPath);
+		const modulesPath = path.join(pkgPath, 'node_modules');
+		const entries = fs.readdirSync(modulesPath).filter((entry) => { return !fs.lstatSync(path.join(modulesPath, entry)).isSymbolicLink(); });
+
+		entries.forEach((entry) => {
+			const entryPath = path.join(modulesPath, entry);
+
+			traverseNodeModules(entryPath, cb);
+
+			cb(entry, entryPath);
+		});
 	}
 
-	catch(err){
-		return; // no node_modules
-	}
-
-	entries = entries.filter(function(entry){
-		const stat = fs.lstatSync(path.join(modulesPath, entry));
-
-		return !stat.isSymbolicLink();
-	});
-
-	entries.forEach(function(entry){
-		const entryPath = path.join(modulesPath, entry);
-
-		traverseNodeModules(entryPath, cb);
-
-		cb(entry, entryPath);
-	});
+	catch(err){ return; }
 }
 
 function rmDir(dirPath, simulate){
@@ -78,20 +56,12 @@ function npmInstall(packagePath, simulate){
 	childProcess.spawnSync(process.platform === 'win32' ? 'npm.cmd' : 'npm', ['i'], { cwd: packagePath, stdio: 'inherit' });
 }
 
-module.exports = function zelda(opts){
-	if(!opts) opts = {};
-
-	// Use folder with nearest package.json as root
+module.exports = function zelda(opts = {}){
 	const rootPath = findRoot(process.cwd());
-
-	const rootName = require(path.join(rootPath, 'package.json')).name;
-	const codePath = path.resolve(rootPath, '..');
-
-	if(!rootName) throw new Error('[zelda] Root package must have a name');
+	const codePath = opts.parentFolder ? path.resolve(opts.parentFolder) : path.resolve(rootPath, '..');
 
 	rmDir(path.join(codePath, 'node_modules'));
 
-	// add node_modules symlink in code folder - MAGIC
 	console.log(`[zelda] cd ${codePath} && ln -s . node_modules`);
 
 	if(!opts.simulate) fs.symlinkSync('.', path.join(codePath, 'node_modules'), 'dir');
@@ -100,24 +70,20 @@ module.exports = function zelda(opts){
 
 	if(opts.install) npmInstall(rootPath, opts.simulate);
 
-	const packagesToPurge = { [rootName]: true };
+	const packagesToPurge = {};
 
-	traverseNodeModules(rootPath, function(packageName, packagePath){
-		if(!codePackages[packageName]) return;
+	traverseNodeModules(rootPath, (packageName) => {
+		if(!codePackages[packageName] || packagesToPurge[packageName]) return;
 
 		packagesToPurge[packageName] = true;
 
 		if(opts.install) npmInstall(path.join(codePath, packageName), opts.simulate);
 
-		if(packagesToPurge[packageName]) rmDir(packagePath, opts.simulate);
+		rmDir(path.join(rootPath, 'node_modules', packageName), opts.simulate);
 	});
 
-	Object.keys(packagesToPurge).forEach(function(packageToPurge){
-		if(packageToPurge === rootName) return;
-
-		const packagePath = path.join(codePath, packageToPurge);
-
-		traverseNodeModules(packagePath, function(packageName, packagePath){
+	Object.keys(packagesToPurge).forEach((packageToPurge) => {
+		traverseNodeModules(path.join(codePath, packageToPurge), (packageName, packagePath) => {
 			if(packagesToPurge[packageName]) rmDir(packagePath, opts.simulate);
 		});
 	});
